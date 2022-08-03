82ec896e32bf8f155364a83350bde4ef11877c56
Raquel Dionísio <dionisio@stadtwerk.org>
2022-08-03 12:35:20 +0200

    Added first exercises

---
+++ hello-world/dist/exercises.js
@@ -0,0 +1,14 @@
+"use strict";
+let a = 100;
+let b = 'Coffee';
+let c = [true, false, false];
+let d = { age: 20 };
+let e = [3];
+let f;
+let g = [];
+let song = {
+    title: 'My song',
+    releaseYear: 1992
+};
+let prices = [100, 200, 300];
+//# sourceMappingURL=exercises.js.map
