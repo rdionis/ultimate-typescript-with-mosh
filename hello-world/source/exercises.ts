b8b7e6776d2cbffbfc8719fd95bd8fe8cdc735fc
Raquel Dionísio <dionisio@stadtwerk.org>
2022-08-09 17:39:28 +0200

    Complete second set of exercises

--- hello-world/source/exercises.ts
+++ hello-world/source/exercises.ts
@@ -26,3 +26,56 @@ let prices = [100, 200, 300];
 
 //function myFunc(a: number, b: number): number { } // there is no return value – A function whose declared type is neither 'void' nor 'any' must return a value.
 
+
+
+// Given the data below, define a type alias for representing users.
+
+type User = {
+    name: string,
+    age: number,
+    occupation?: string,
+}
+
+let users: User[] = [
+    {
+        name: 'John Smith',
+        age: 30,
+        occupation: 'Software engineer'
+    }, {
+        name: 'Kate Müller',
+        age: 28
+    }
+];
+
+// • Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+
+
+type Bird = {
+    fly: () => void,
+}
+
+type Fish = {
+    swim: () => void,
+}
+
+type Pet = Bird | Fish
+
+// • Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”, etc.
+
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
+
+// • Simplify the following code snippets:
+
+let user = getUser();
+console.log(user?.address?.street);
+
+let x = foo ?? bar()
+
+
+// • What is the problem in this piece of code?
+
+let value: unknown = 'a';
+if (typeof value === 'string')
+    console.log(value.toUpperCase());
+
+// we get a compilation error since the method 'toUpperCase' can't be called on a type unknown object – we need to use 'narrowing'
