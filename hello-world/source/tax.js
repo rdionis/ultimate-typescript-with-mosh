5fc94c954f4849daab1c101ec166d8744b59151c
Raquel Dionísio <dionisio@stadtwerk.org>
2022-08-17 16:37:41 +0200

    JSDocs

--- hello-world/source/tax.js
+++ hello-world/source/tax.js
@@ -1,6 +1,19 @@
-// @ts-nocheck
+// // @ts-nocheck
 // we add this comment once on the top of a file that we don't want the TS compiler to check
 
+
+// if we type /** */ VSCode will detect it as a JSDoc comment – so we can just press 'Enter' to generate the comment
+
+/**
+ * Calculates income tax.
+ * // @ param {*} income // we see here we have a parameter called 'income' and between {} we have the type of the parameter
+ * // at this point, VSCode does not know the type of this parameter, which is why we have an * here
+ * // we have to do that explicitly:
+ * @param {number} income
+ * // @ returns // here, we can also specify the type of the return value:
+ * @returns {number}
+ */
+// using JSDoc, we can provide type information to our TS compiler, but we can also explain our code – see line 8
 export function calculateOtherTax(income) {
     return income * .3
 }
