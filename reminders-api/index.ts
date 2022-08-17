57127975219051253bd735e6c378b74219eafd3f
Raquel Dionísio <dionisio@stadtwerk.org>
2022-08-17 17:48:39 +0200

    Node with TypeScript

---
+++ reminders-api/index.ts
@@ -0,0 +1,4 @@
+let x: number = 1; // returns error: "let x: number = 1; SyntaxError: Unexpected token ':'"
+console.log(x)
+
+// node is an execution engine for JS code and cannot understand TS code – node cannot execute TS
